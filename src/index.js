35484766795ccad4d152f6eac2492d3798816ad2
kagera <52308117+kagerka@users.noreply.github.com>
2021-02-19 17:57:07 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    if (str.length % 2 == 1) {
+        return false;
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+                stack.push(str[i]);
+            } else if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                if (stack[stack.length - 1] !== bracketsConfig[j][0] || stack.length == 0) {
+                    stack.push(str[i]);
+                } else {
+                    stack.pop();
+                }
+            } else if (str[i] == bracketsConfig[j][1]) {
+                if (stack[stack.length - 1] !== bracketsConfig[j][0]) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+    }
+
+    if (stack.length !== 0 && stack[0] != stack[stack.length - 1]) {
+        return false;
+    }
+    if (stack.length == 0 || stack.map((a, b) => a == b)) {
+        return true;
+    } else {
+        return false;
+    }
 }
